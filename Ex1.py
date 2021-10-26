272a2ffaa7b9572d5edba9f66164903ea5dd0531
Chiperi <iulianchiperi2000@gmail.com>
2021-10-26 13:59:08 +0300

    init

---
+++ Ex1.py
@@ -0,0 +1,8 @@
+#Cerinta: Scrieți o singură linie de cod, folosind funcția print(), astfel încât să afișați următoarele linii, în consolă:
+#"I'm"
+#" "learning" "
+#" " "Python" " "
+
+#Rezolvare:
+
+print("I'm \n learning \n  Python")
