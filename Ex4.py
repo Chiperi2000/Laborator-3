031a5d77d62ea06f4d065f5e525346f86d337c40
Chiperi <iulianchiperi2000@gmail.com>
2021-10-26 15:58:42 +0300

    rezolvat

---
+++ Ex4.py
@@ -0,0 +1,7 @@
+#cod care afișează poziția Like-ului median
+
+test = "iiiLikePythonLikeILikeIcecream"
+print("Pozitia Like-ului de inceput: ", test.index('Like'))
+print("Pozitia Like-ului de final: ",test.rindex('Like'))
+print("Pozitia Like-ului median: ",test.rindex('Like',3 , 18))
+
