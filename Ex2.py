28ba81e9dbf87d24a70badadd739004946c608a4
Chiperi <iulianchiperi2000@gmail.com>
2021-10-26 14:24:48 +0300

    commit

--- Ex2.py
+++ Ex2.py
@@ -1,1 +1,10 @@
 #Cerinta: Scrieți un program care va determina valoarea ipotenuzei unui triunghi dreptunghic, valorile catetelor citindu-se de la tastatură.
+
+#Rezolvare:
+import math
+cateta1 = input("Valoarea catetei 1 este:")
+a=int(cateta1)
+cateta2 = input("Valoarea catetei 2 este:")
+b=int(cateta2)
+z = math.sqrt(a ** 2 + b ** 2)
+print(z)
