7a6530e3a2090daedfc624cbb225440005420522
Chiperi <iulianchiperi2000@gmail.com>
2021-10-26 15:31:41 +0300

    commit

--- Ex3.py
+++ Ex3.py
@@ -1,1 +1,7 @@
-#Citiți de la tastatură numele și prenumele într-un singur text și afișați separat, pe câte un rând numele și prenumele.
+#Cerinta: Citiți de la tastatură numele și prenumele într-un singur text și afișați separat, pe câte un rând numele și prenumele.
+
+#Rezolvare:
+
+Numele_si_prenumele = input("Scrieti numele si prenumele: ")
+print("Numele:",Numele_si_prenumele.split(" ",1)[0] )
+print("Prenumele:",Numele_si_prenumele.split(" ",1)[1] )
